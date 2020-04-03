4ea838d64d243f2a666fc5d20b9468020b9f3adb
MarinaBartulov <bartulovmarina1@gmail.com>
2020-04-03 11:45:09 +0200

    The skeleton of the project created.

---
+++ bsep-back/src/main/java/team19/project/utils/KeyStoreReader.java
@@ -0,0 +1,126 @@
+package team19.project.utils;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder;
+import team19.project.model.IssuerData;
+
+public class KeyStoreReader {
+
+
+    private KeyStore keyStore;
+
+    public KeyStoreReader() {
+        try {
+            keyStore = KeyStore.getInstance("JKS", "SUN");
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param keyStoreFile - datoteka odakle se citaju podaci
+     * @param alias - alias putem kog se identifikuje sertifikat izdavaoca
+     * @param password - lozinka koja je neophodna da se otvori key store
+     * @param keyPass - lozinka koja je neophodna da se izvuce privatni kljuc
+     * @return - podatke o izdavaocu i odgovarajuci privatni kljuc
+     */
+    public IssuerData readIssuerFromStore(String keyStoreFile, String alias, char[] password, char[] keyPass) {
+        try {
+
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(keyStoreFile));
+            keyStore.load(in, password);
+            Certificate cert = keyStore.getCertificate(alias);
+            PrivateKey privKey = (PrivateKey) keyStore.getKey(alias, keyPass);
+
+            X500Name issuerName = new JcaX509CertificateHolder((X509Certificate) cert).getSubject();
+            return new IssuerData(privKey, issuerName);
+
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public Certificate readCertificate(String keyStoreFile, String keyStorePass, String alias) {
+        try {
+
+            KeyStore ks = KeyStore.getInstance("JKS", "SUN");
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(keyStoreFile));
+            ks.load(in, keyStorePass.toCharArray());
+
+            if(ks.isKeyEntry(alias)) {
+                Certificate cert = ks.getCertificate(alias);
+                return cert;
+            }
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public PrivateKey readPrivateKey(String keyStoreFile, String keyStorePass, String alias, String pass) {
+        try {
+            KeyStore ks = KeyStore.getInstance("JKS", "SUN");
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(keyStoreFile));
+            ks.load(in, keyStorePass.toCharArray());
+
+            if(ks.isKeyEntry(alias)) {
+                PrivateKey pk = (PrivateKey) ks.getKey(alias, pass.toCharArray());
+                return pk;
+            }
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
