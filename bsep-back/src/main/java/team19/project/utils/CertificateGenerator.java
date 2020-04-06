2ede1ebc0d147ca8b50bc9145c86433d4dc0cf7e
Aleksandra Randjelovic <aleksandra.randjelovic19@gmail.com>
2020-04-06 22:36:59 +0200

    Added extensions

--- bsep-back/src/main/java/team19/project/utils/CertificateGenerator.java
+++ bsep-back/src/main/java/team19/project/utils/CertificateGenerator.java
@@ -10,6 +10,7 @@ import java.security.Security;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.List;
 
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
@@ -24,7 +25,8 @@ public class CertificateGenerator {
 
     public CertificateGenerator() {}
 
-    public X509Certificate generateCertificate(SubjectData subjectData, IssuerData issuerData, boolean isCa, int keyUsage) {
+    public X509Certificate generateCertificate(SubjectData subjectData, IssuerData issuerData, boolean isCa,
+                                               List<Integer> keyUsageList, List<String> exKeyUsageList) {
         Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
         try {
             JcaContentSignerBuilder builder = new JcaContentSignerBuilder("SHA256WithRSAEncryption");
@@ -40,7 +42,19 @@ public class CertificateGenerator {
                     subjectData.getPublicKey());
 
             certGen.addExtension(Extension.basicConstraints, false, new BasicConstraints(isCa));
-            certGen.addExtension(Extension.keyUsage, true, new KeyUsage(keyUsage));
+
+            for (Integer i : keyUsageList) {
+                certGen.addExtension(Extension.keyUsage, true, new KeyUsage(i));
+            }
+
+            if(exKeyUsageList != null) {
+                for (String s : exKeyUsageList) {
+                    ASN1ObjectIdentifier newKeyPurposeIdOID = new ASN1ObjectIdentifier(s);
+                    KeyPurposeId newKeyPurposeId = KeyPurposeId.getInstance(newKeyPurposeIdOID);
+                    ExtendedKeyUsage eku = new ExtendedKeyUsage(newKeyPurposeId);
+                    certGen.addExtension(Extension.extendedKeyUsage, false, eku);
+                }
+            }
 
             X509CertificateHolder certHolder = certGen.build(contentSigner);
             JcaX509CertificateConverter certConverter = new JcaX509CertificateConverter();
