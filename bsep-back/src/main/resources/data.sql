3b58bed79749454b6b70ce3de9ed09017d046a91
Aleksandra Randjelovic <aleksandra.randjelovic19@gmail.com>
2020-04-15 11:01:57 +0200

    Data.sql is corrected

--- bsep-back/src/main/resources/data.sql
+++ bsep-back/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into revocation_reason (name) value ('Public key compromised');
+insert into revocation_reason (name) value ('Private key compromised');
 insert into revocation_reason (name) value ('Invalid request for issuing certificate');
 insert into revocation_reason (name) value ('Unintentionally signed certificate');
 insert into revocation_reason (name) value ('Subject misconduct');
